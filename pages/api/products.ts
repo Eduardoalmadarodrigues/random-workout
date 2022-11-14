2ce69bf2937537b3c2542abbb6b286a3eaa45e01
noziX <94942615+noziXsHooter@users.noreply.github.com>
2022-11-14 19:55:17 -0300

    changes

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,9 +1,37 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import { MongoClient, Db } from 'mongodb';
 
- type Data = {
-  name: string,
-} 
+let cachedDb: Db;
 
+ async function connectToDatabase(uri: string){
+
+  if(cachedDb) {
+    return cachedDb;
+  }
+
+  const client = await MongoClient.connect(uri);
+
+  const db = client.db('ecommercetest');
+
+  cachedDb = db;
+
+  return db;
+  }
+
+   export default async (req: VercelRequest ,res: VercelResponse) =>{
+
+    const uri = 'mongodb+srv://ecommercetest:ecommercetest@ecommercetest.hlmhtv7.mongodb.net/?retryWrites=true&w=majority';
+
+    const db = await connectToDatabase(uri);
+
+    const collection = db.collection('products');
+
+    const list = await collection.find().toArray();
+
+    return res.status(201).json(list);
+  }
+
+/* 
 // Require and initialize outside of your main handler
 const mysql = require('serverless-mysql')({
     config: {
@@ -14,13 +42,16 @@ const mysql = require('serverless-mysql')({
     }
   })
 
-  export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
-  ) {
-    let results = mysql.query('SELECT * FROM products')
-    mysql.end()
-    res.status(200).json(results)
-  }
+ 
+   export default function (req: VercelRequest ,res: VercelResponse) {
 
+    async function connectToDatabse(){
 
+      let results = await mysql.connect()
+    
+      res.send(results);
+      }
+     var { name } = req.body;
+    res.send({ message: `Este é o produto: ${name}`});
+    }
+  */
